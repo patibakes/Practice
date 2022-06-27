77eff1087954f134cdce1ef773d61ed91144fe88
patibakes <cepatia4@gmail.com>
2022-06-27 21:17:11 +0200

    second exercise from basics_part-1

---
+++ w3resource/basics_part-I/002.py
@@ -0,0 +1,9 @@
+# Importuję bibliotekę python 'sys' która zawiera informacje systemowe.
+import sys
+
+#Wyświetlam wersję pythona
+print("python version")
+print(sys.version)
+print("version info.")
+print(sys.version_info)
+
